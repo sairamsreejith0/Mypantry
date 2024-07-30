bbd923a11da072d925d13ad08757b63a20e3ce81
sairamsreejith0 <sairamsreejith0@gmail.com>
2024-07-30 14:25:10 -0500

    logo added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,14 @@
 "use client";
 import React, { useState } from "react";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import {
+  AppBar,
+  Toolbar,
+  Typography,
   Box,
   Grid,
   TextField,
@@ -13,91 +21,161 @@ import {
   IconButton,
   TableContainer,
   Paper,
+  InputBase,
+  alpha,
 } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import SearchIcon from "@mui/icons-material/Search";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
 export default function Home() {
-  const [items, addItems] = useState([]);
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState("");
 
   const handleAddClick = () => {
     if (newItem.trim() !== "") {
-      addItems([...items, newItem]);
+      setItems([...items, newItem]);
       setNewItem("");
     }
   };
+
   const handleInputChange = (event) => {
     setNewItem(event.target.value);
   };
 
   const handleDeleteClick = (index) => {
-        const newItems = [...items]
-        newItems.splice(index,1);
-        addItems(newItems); 
-      
-  }
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  };
 
   return (
-    <Box
-      height="100vh"
-      width="100vw"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      bgcolor=""
-    >
-      <Box height="500px" width="500px" bgcolor="" padding={2} overflow="auto">
-        <Grid container spacing={2} alignItems="center" justifyContent="center">
-          <Grid item xs={12} md={8}>
-            <TextField
-              fullWidth
-              id="outlined-basic"
-              label="Outlined"
-              variant="outlined"
-              value={newItem}
-              onChange={handleInputChange}
+    <>
+      <AppBar position="static" sx={{ bgcolor: "#7C73C0" }}>
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            <img
+              src="/logo.png" // Replace with your logo URL
+              alt="Logo"
+              style={{ marginRight: "10px", width: "110px", height: "40px" }}
             />
-          </Grid>
-          <Grid item xs={12} md={3}>
-            <Button variant="contained" fullWidth onClick={handleAddClick}>
-              Add
-            </Button>
-          </Grid>
-        </Grid>
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
 
-        <TableContainer component={Paper} style={{ marginTop: 20 }}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Pantry Item</TableCell>
-                <TableCell align="center">Edit</TableCell>
-                <TableCell align="center">Delete</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {items.map((item,index) => (
-                <TableRow key={index}>
-                  
-                  <TableCell component="th" scope="row">
-                    {item}
-                  </TableCell>
-                  <TableCell align="center">
-                    <IconButton color="primary">
-                      <EditIcon />
-                    </IconButton>
-                  </TableCell>
-                  <TableCell align="center">
-                    <IconButton sx={{ color: "red" }} onClick={() => handleDeleteClick(index)}>
-                      <DeleteIcon />
-                    </IconButton>
-                  </TableCell>
+      <Box
+        height="100vh"
+        width="100vw"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        bgcolor=""
+      >
+        <Box height="500px" width="700px" bgcolor="" padding={2} overflow="auto">
+          <Grid container spacing={2} alignItems="center" justifyContent="center">
+            <Grid item xs={12} md={8}>
+              <TextField
+                fullWidth
+                id="outlined-basic"
+                label="enter item name"
+                variant="outlined"
+                value={newItem}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <Button variant="contained" fullWidth onClick={handleAddClick} sx={{bgcolor:"#7C73C0"}}>
+                Add
+              </Button>
+            </Grid>
+          </Grid>
+
+          <TableContainer component={Paper} style={{ marginTop: 20 }}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell><Typography variant='h5'>Pantry Item</Typography></TableCell>
+                  <TableCell align="center"><Typography variant='h5'>Edit</Typography></TableCell>
+                  <TableCell align="center"><Typography variant='h5'>Delete</Typography></TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {items.map((item,index) => (
+                  <TableRow key={index}>
+                    
+                    <TableCell component="th" scope="row"><Typography variant = "h6">{item}</Typography>
+                      
+                    </TableCell>
+                    <TableCell align="center">
+                      <IconButton color="primary">
+                        <EditIcon />
+                      </IconButton>
+                    </TableCell>
+                    <TableCell align="center">
+                      <IconButton sx={{ color: "red" }} onClick={() => handleDeleteClick(index)}>
+                      <DeleteIcon fontSize="small"/>
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
